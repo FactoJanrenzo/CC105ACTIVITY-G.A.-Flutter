f16a4ad3b5d89a09a7d94ed48be896eade20a9f6
Renzo <janrenzofacto@gmail.com>
2020-11-03 16:38:20 +0800

    run done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.blueGrey,
+            appBar: AppBar(
+              title: Text('I am Rich'),
+              backgroundColor: Colors.blueGrey[900],
+            ),
+            body: Center(
+              child: Image(
+                image: NetworkImage('images/diamond.png'),
+              ),
+            ))),
+  );
 }
+   
